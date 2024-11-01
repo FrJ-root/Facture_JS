70a19019cb4f4781c78d8b95ba98064c15495358
FrJ-root <frj.mitnick@gmail.com>
2024-11-01 16:06:29 +0100

    lblbala

---
+++ invoice.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const invoiceTable = document.getElementById('invoiceTable').getElementsByTagName('tbody')[0];
+    const invoiceTotal = document.getElementById('invoiceTotal');
+
+
+    let Asset1 = document.getElementById("input").value
+
+    let Asset2 = document.getElementById("input").value;
+    let ar = Asset1*Asset2;
+    document.getElementById("solition1").innerHTML.value = solition1 ;
+    console.log(test);
+document.createAttribute()   
+    
+    var Asset3 = document.getElementById("input").value;
+    var ar = document.getElementById("input").value;
+    var ar = Asset1 * Asset2;
+    document.getElementById("ar1").value = solition2;
+    
+    var Asset5 = document.getElementById("input").value;
+    var Asset6 = document.getElementById("input").value;
+    var ar = Asset1 * Asset2;
+    document.getElementById("solition1").value = solition3;
+
+
+    // This function updates the total amount in each row ( item price * Qte )
+    function updateItemTotal(row) {
+       
+    }
+
+
+    // This function updates the total amount of the invoice (sum all Item totals)
+    function updateInvoiceTotal() {
+       
+    }
+
+
+    // Ajouter un nouveau item 
+    function addNewItem() {
+        
+
+    }
+
+
+    // If you finish the above functions, consider adding a delete button
+ 
+});
